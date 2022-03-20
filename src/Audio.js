dbbb41a35fd60819588b67bda7509817537e5a1d
judithrauland <97590487+judithrauland@users.noreply.github.com>
2022-03-20 15:42:33 +0100

    Added audio function

---
+++ src/Audio.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Audio.css";
+
+export default function Audio(props) {
+  return (
+    <div className="Audio">
+      <audio className="audioFile" src={props.audio}></audio>
+      <span
+        className="btn"
+        onClick={function () {
+          const audio = document.getElementsByClassName("audioFile")[0];
+          audio.play();
+        }}
+      >
+        <i className="fas fa-head-side-cough"></i>
+      </span>
+    </div>
+  );
+}
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,13 @@
 import React from "react";
+import Audio from "./Audio";
 import "./Phonetic.css";
 
-export default function Phonetic(props) {
+export default function Phonetics(props) {
   return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        {" "}
-        Listen
-      </a>
+    <div className="AllPhonetics">
+      <span className="playSound">
+        <Audio audio={props.phonetic.audio} />
+      </span>
       <span className="text">{props.phonetic.text}</span>
     </div>
   );
